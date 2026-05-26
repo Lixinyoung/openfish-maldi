a84609e3775beffd31d11269e08dbccf07eb7220
李心杨 <76689104+Lixinyoung@users.noreply.github.com>
2026-05-26 13:44:14 +0800

    Delete Decoding directory

--- Decoding/Windows/openDecode_Black/utils.py
+++
@@ -1,7 +0,0 @@
-
-
-def is_all_true(data:dict) -> bool:
-    """check dict is all True"""
-    if isinstance(data, dict):
-        return all(is_all_true(value) for value in data.values())
-    return data is True
